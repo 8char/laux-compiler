ef8de0ff6adc7299bd7c1b02dd512885ed07c20f
Matteo Krans Dusic <matteokrantz@gmail.com>
2023-07-01 23:00:16 +0200

    feat: add compile export for dependents

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 require("babel-core/register");
-require("./src/app.js");
+
+import compiler from "../../src/compiler";
+import parser from "../../src/parser";
+import CodeGenerator from "../../src/codegenerator";
+
+export function compile(code) {
+    const compiledAST = compiler.compile(
+        {...parser.parse(code)},
+        {
+            debug: false
+        }
+    )
+
+    return new CodeGenerator(code, compiledAST)
+        .generate()
+        .code;
+}
