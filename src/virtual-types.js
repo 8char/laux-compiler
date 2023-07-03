7cf93a395426baaa45f9c733c7766fe17c54fc55
Matteo Krans Dusic <matteokrantz@gmail.com>
2023-07-03 17:24:57 +0200

    refactor: remove unecessary arg

--- src/virtual-types.js
+++ src/virtual-types.js
@@ -2,7 +2,7 @@ import * as t from './types';
 
 export const ReferencedIdentifier = {
   types: ['Identifier'],
-  checkPath({ node, parent }, opts) {
+  checkPath({ node, parent }) {
     // check if node is referenced
     return t.isReferenced(node, parent);
   },
