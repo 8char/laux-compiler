9aef8087b8b61be5723a07c70738e6b5c1ca5240
8char <matteokrantz@gmail.com>
2023-08-08 16:22:49 +0200

    fix: add changelogs to each new tagged update

--- release.config.cjs
+++ release.config.cjs
@@ -5,11 +5,17 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "docs/CHANGELOG.md",
+      },
+    ],
+    [
       "@semantic-release/git",
       {
         assets: ["package.json", "CHANGELOG.md"],
-        // eslint-disable-next-line no-template-curly-in-string
         message:
+          // eslint-disable-next-line no-template-curly-in-string
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
